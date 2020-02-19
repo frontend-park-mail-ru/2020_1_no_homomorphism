2455fa5048b0e0535b38a48798a86b9f15111b46
tmible <panevnikita@inbox.ru>
2020-02-19 12:49:02 +0300

    server 0.1 Подключил express и nunjuсks, написал простой сервер, отдающий странички

--- server.js
+++ server.js
@@ -1,16 +1,47 @@
-const http = require('http');
-const routing = require('./routing');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const nunjucks = require('nunjucks');
 
-let server = new http.Server(function(req, res) {
-    var jsonString = '';
-    res.setHeader('Content-Type', 'application/json');
-    req.on('data', (data) => {
-        jsonString += data;
-    });
+const index = require('./routes/index');
+const login = require('./routes/login');
 
-    req.on('end', () => {
-        routing.define(req, res, jsonString);
-    });
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'njk');
+
+app.use(favicon(path.join(__dirname, 'static', 'favicon.ico')));
+app.use(logger('dev'));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'static')));
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
 });
 
-server.listen(9018, 'localhost');
+app.use('/', index)
+app.use('/login', login)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(3000);
