4c02f54ac62fb125e4e2adb96f9d49c1f7532a83
tmible <panevnikita@inbox.ru>
2020-02-20 17:36:45 +0300

    server 0.2.2 Фикс

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use((err, req, res, next) => {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {error: err});
 });
 
 app.listen(3000);
