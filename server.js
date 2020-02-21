4ec718bd644d483036f5d69ff229af1fe05463e5
tmible <52622566+tmible@users.noreply.github.com>
2020-02-21 17:49:39 +0300

    server.js 1.0.1

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/login', (req, res) => {
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  let err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
