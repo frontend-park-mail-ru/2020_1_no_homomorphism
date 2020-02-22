ae66a750baa762edbfebd3d790861b2a5a01697b
tmible <panevnikita@inbox.ru>
2020-02-22 21:44:30 +0300

    Установлен и настроен ESlint

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ nunjucks.configure('views', {
 });
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index');
 });
 
 app.get('/login', (req, res) => {
@@ -27,20 +27,20 @@ app.get('/login', (req, res) => {
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error', {error: err});
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error', {error: err});
 });
 
 app.listen(3000);
