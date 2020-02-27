9def7b411759c6f3ea10fe1ef2d0763727fb8ab6
tmible <panevnikita@inbox.ru>
2020-02-27 05:15:22 +0300

    CORS try 3

--- static/js/models/player.js
+++ static/js/models/player.js
@@ -63,14 +63,17 @@ export class PlayerModel {
 
     getFirst() {
         Api.trackFetch('12345')
-        .then((res) => {
+        .then(response => response.json())
+        .then‌(data => { console.log(data); });
+        /*.then((res) => {
             if (res.ok) {
+                console.log(res);
                 const track = JSON.parse(res);
                 document.getElementsByTagName('audio')[0].children[0].src = track.link;
                 document.getElementsByTagName('audio')[0].load();
                 this.eventBus.emit('track update', track);
             }
-        });
+        });*/
         // document.getElementsByTagName('audio')[0].children[0].src = this.data.playlist[this.data.queue[this.data.current]].src;
         // document.getElementsByTagName('audio')[0].load();
         // this.eventBus.emit('track update', this.data.playlist[this.data.queue[this.data.current]]);
