fa6765edb3b6e08a898b6e1916aa1fd71865c4be
Tataklim <tatakaklim@gmail.com>
2020-02-28 14:13:33 +0300

    [FIX]valid

--- static/js/modules/validation.js
+++ static/js/modules/validation.js
@@ -8,6 +8,9 @@ export  class Validation {
      * @returns {String} error Пустая строчка в случае корректных данных, иначе - текст ошибки
      */
     validationEmail(email = '') {
+        if (email === '') {
+            return 'Введите почту'
+        }
         let regExpr = new RegExp('^[a-z0-9_!#$%&+=*\\-]+(\.[a-z0-9_\\-]*)*@([a-z0-9][a-z0-9]+\.)*[a-z]{2,4}(\.[a-z]{2,4})+$');
         if (!email.match(regExpr)) {
             return 'Некорректная почта';
@@ -19,9 +22,9 @@ export  class Validation {
      * @returns {String} error Пустая строчка в случае корректных данных, иначе - текст ошибки
      */
     validationLogin (login = '') {
-        let regExpr = new RegExp('^[a-zA-Z][a-zA-Z0-9_.]{4,14}$');
+        let regExpr = new RegExp('^[a-zA-Z0-9_.]{3,}$');
         if (!login.match(regExpr)) {
-            return 'Логин может содержать латинские буквы или цифры';
+            return 'Логин может содержать от 3 латинских букв или цифр';
         }
         return '';
     }
@@ -31,14 +34,23 @@ export  class Validation {
      * @returns {String} error Пустая строчка в случае корректных данных, иначе - текст ошибки
      */
     validationPassword(password1 = '', password2 = '') {
+
+        if(password2 === ''){
+            return '';
+        }
+
         if (password1 !== password2) {
             return 'Пароли не совпадают';
         }
 
-        let regExpr = new RegExp('^[a-zA-Zd]{7,}$');
+        let regExpr = new RegExp('^[a-zA-Z0-9]{3,}$');
+
+        if (password1 === '') {
+            return 'Введите пароль';
+        }
 
         if (!password1.match(regExpr)) {
-            return 'Пароль должен содержать не менее 7 заглавных или строчных букв';
+            return 'Пароль должен содержать не менее 3 заглавных или строчных букв';
         }
         return '';
     }
