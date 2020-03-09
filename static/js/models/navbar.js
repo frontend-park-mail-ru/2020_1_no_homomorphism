ba98fe3cbd77e77641167296bd556ef4b2a6c927
tmible <panevnikita@inbox.ru>
2020-03-09 15:00:41 +0300

    Cookie fetch

--- static/js/models/navbar.js
+++ static/js/models/navbar.js
@@ -14,6 +14,8 @@ export class NavbarModel {
         this.globalEventBus = globalEventBus;
         this.eventBus.on('get user data', this.getUserData.bind(this));
         this.eventBus.on('logout', this.logout.bind(this));
+
+        Api.cookieFetch();
     }
 
     /**
