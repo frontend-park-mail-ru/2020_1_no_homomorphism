afa6da147e5148a4f849d7fb5768c6e0cff963ad
tmible <panevnikita@inbox.ru>
2020-05-18 19:18:44 +0300

    Фиксы главной

--- static/js/views/news/news.js
+++ static/js/views/news/news.js
@@ -19,6 +19,9 @@ export default class NewsView extends BaseView {
         this.eventBus.on(MAIN.RENDER_TRACKS_LIST, this.renderList.bind(this));
         this.eventBus.on(MAIN.RENDER_ARTISTS, this.renderList.bind(this));
         globalEventBus.on(GLOBAL.HIDE_SUBSCRIPTIONS, () => {
+            if (!document.getElementsByClassName('subscriptions-section')[0]) {
+                return;
+            }
             document.getElementsByClassName('subscriptions-section')[0]
                 .parentNode.parentNode.remove();
         });
@@ -47,6 +50,9 @@ export default class NewsView extends BaseView {
         }
         node.innerHTML = newsSection(data);
         node.classList.remove(data.domItem);
+        if (!data.ok) {
+            return;
+        }
         node.firstChild.lastChild.classList.add(data.domItem);
     }
 }
